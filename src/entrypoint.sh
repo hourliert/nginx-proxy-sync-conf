f8898f3b9c871c1a298df59940767757954435f5
Thomas Hourlier <thomas.hourlier@cnode.fr>
2016-07-12 11:28:22 +0200

    feat(entyrpoint): support multiple vhost

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
 
+IFS=',' read -ra HOSTS <<< "$VIRTUAL_HOST"
+
 echo "Copying config into nginx vhost.d and htpasswd volumes"
 
-if [ -e "/config/htpasswd" ]
-then
-  cp /config/htpasswd /htpasswd/$VIRTUAL_HOST
-fi
+for i in "${!HOSTS[@]}"; do
+  HOST="${HOSTS[$i]}"
 
-if [ -e "/config/vhost" ]
-then
-  cp /config/vhost /vhost.d/$VIRTUAL_HOST
-fi
+  echo "Configuring $HOST"
 
-if [ -e "/config/vhost_location" ]
-then
-  cp /config/vhost_location /vhost.d/$VIRTUAL_HOST_location
-fi
+  if [ -e "/config/htpasswd" ]
+  then
+    cp /config/htpasswd /htpasswd/$HOST
+  fi
+
+  if [ -e "/config/vhost" ]
+  then
+    cp /config/vhost /vhost.d/$HOST
+  fi
+
+  if [ -e "/config/vhost_location" ]
+  then
+    cp /config/vhost_location /vhost.d/$HOST_location
+  fi
+
+done
 
 echo "Done"
